fd39511df3a5ca69d9800a320bb78f4e62b38353
Andrew Heim <dheim@tonicdesign.com>
2016-08-12 10:07:16 -0400

    first commit

---
+++ datePicker/ViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var monthTable: UITableView!
+    @IBOutlet weak var dayTable: UITableView!
+    @IBOutlet weak var yearTable: UITableView!
+
+    var calendar: NSCalendar {
+        get {
+            return NSCalendar.currentCalendar()
+        }
+    }
+    var currentDate = NSDate()
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == monthTable {
+            return calendar.monthSymbols.count
+        } else if tableView == dayTable {
+        let days = calendar.rangeOfUnit(.Day, inUnit: .Month, forDate: currentDate)
+            return days.length
+        } else  {
+            return 3
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell?
+        if tableView == monthTable {
+            cell = tableView.dequeueReusableCellWithIdentifier("month")
+            let components = NSDateComponents()
+            components.setValue(indexPath.row, forComponent: .Month)
+            
+        } else if tableView == dayTable {
+            cell = tableView.dequeueReusableCellWithIdentifier("day")
+        } else {
+            cell = tableView.dequeueReusableCellWithIdentifier("year")
+        }
+        return cell!
+    }
+}
